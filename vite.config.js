329db2b0bcbed2c07b0fb6496efe4b888655848e
itskendel <itskendelantar@gmail.com>
2025-10-03 02:54:48 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import {defineConfig} from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import {resolve} from 'path'
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
-
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                profile: resolve(__dirname, 'profile.html'),
-                project: resolve(__dirname, 'project.html'),
-                skill: resolve(__dirname, 'skill.html')
-            }
-        }
-    }
 })
